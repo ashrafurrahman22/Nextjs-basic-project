4710e2336c0f69086d087829b69a576e0dee7474
ashrafur22 <ashrafurrahman22@gmail.com>
2022-06-01 20:01:13 +0600

    last

---
+++ pages/dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const dashboard = () => {
+
+
+    return (
+        <div>
+            <h2>this is dashboard bro. </h2>
+        </div>
+    );
+};
+
+export default dashboard;
---
+++ pages/users/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const index = ({users}) => {
+    return (
+        <div>
+            <h2>this is users: {users.length} </h2>
+        </div>
+    );
+};
+
+export default index;
+
+export async function getStaticProps(context) {
+    const res  = await fetch ('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json();
+
+    return {
+      props: {users: data}, // will be passed to the page component as props
+    }
+  }
