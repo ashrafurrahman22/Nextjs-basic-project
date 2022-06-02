19e24d46143b365ef87323c6f5165d4cc4c3a78a
ashrafur22 <ashrafurrahman22@gmail.com>
2022-06-02 10:11:57 +0600

    last

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,9 +1,22 @@
+import Link from 'next/link';
 import React from 'react';
 
 const index = ({users}) => {
     return (
         <div>
             <h2>this is users: {users.length} </h2>
+            <div>
+                {
+                    users.map(user => 
+                        <div style={{ 'border': '1px solid red', 'padding': '15px', borderRadius: '10px', 'margin': '10px' }}  key={user.id}>
+                            <h4>UserName: {user.name}</h4>
+                            <Link href={`/users/${user.id}`}>
+                                <button>Explore</button>
+                            </Link>
+                        </div>
+                        )
+                }
+            </div>
         </div>
     );
 };
